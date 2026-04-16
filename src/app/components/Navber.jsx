3ca899091c9dbfceb43b8b074ecb68ecb4079691
fahimad24 <mdtaraquerahmanfahim@gmail.com>
2026-04-16 22:38:16 +0600

    complete Timeline page and add fillter funtion

--- src/app/components/Navber.jsx
+++ src/app/components/Navber.jsx
@@ -1,9 +1,20 @@
+"use client";
+
 import Link from "next/link";
 import { RiHome2Line } from "react-icons/ri";
 import { WiTime3 } from "react-icons/wi";
 import { GoGraph } from "react-icons/go";
+import { usePathname } from "next/navigation";
 
 const Navber = () => {
+  const param = usePathname();
+  console.log(param);
+
+  const navItems = [
+    { name: "Home", href: "/", icon: <RiHome2Line /> },
+    { name: "Timeline", href: "/timeline", icon: <WiTime3 /> },
+    { name: "Stats", href: "/stats", icon: <GoGraph /> },
+  ];
   return (
     <nav className="bg-base-100 shadow-md">
       <div className="container mx-auto flex justify-between py-5 ">
@@ -14,33 +25,17 @@ const Navber = () => {
         </div>
         <div className="hidden md:flex">
           <ul className="menu menu-horizontal gap-2 p-0 text-lg text-gray-600">
-            <li>
-              <Link
-                className="flex gap-2 justify-center py-1.5 px-3 rounded-md bg-green text-white hover:bg-green/90"
-                href="/"
-              >
-                <RiHome2Line />
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                className="flex gap-2 justify-center py-1.5 px-3 rounded-md "
-                href="/timeline"
-              >
-                <WiTime3 />
-                Timeline
-              </Link>
-            </li>
-            <li>
-              <Link
-                className="flex gap-2 justify-center py-1.5 px-3 rounded-md"
-                href="/stats"
-              >
-                <GoGraph />
-                Stats
-              </Link>
-            </li>
+            {navItems.map((item) => (
+              <li key={item.href}>
+                <Link
+                  className={`flex gap-2 justify-center py-1.5 px-3 rounded-md ${param === item.href ? "bg-green text-white hover:bg-green/90" : "bg-white text-gray-600 hover:bg-green/90 hover:text-white"}`}
+                  href={item.href}
+                >
+                  {item.icon}
+                  {item.name}
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
--- src/app/timeline/page.jsx
+++ src/app/timeline/page.jsx
@@ -1,8 +1,76 @@
+"use client";
+
+import { useContext, useState } from "react";
+import { AuthContext } from "@/context/contextApi";
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
+
 const Timeline = () => {
+  const { timeline } = useContext(AuthContext);
+  const [historyLog, setHistoryLog] = useState(timeline);
+
+  const handleFilter = (type) => {
+    if (type === "All") {
+      setHistoryLog(timeline);
+    } else {
+      const filteredLog = timeline.filter((entry) => entry.type === type);
+      setHistoryLog(filteredLog);
+    }
+  };
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold text-center mt-10">Timeline Page</h1>
-    </div>
+    <main>
+      <section className=" container mx-auto ">
+        <div className="py-20">
+          <h1 className="text-5xl font-bold">Timeline</h1>
+          {/* dropdown */}
+          <div className="dropdown mt-5">
+            <label
+              tabIndex={0}
+              className="flex gap-2 justify-between py-1.5 px-3 rounded-md bg-green text-white hover:bg-green/90 w-50"
+            >
+              <span>Filter timeline</span>
+              <MdOutlineKeyboardArrowDown />
+            </label>
+            <ul
+              tabIndex={0}
+              className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a onClick={() => handleFilter("All")}>All</a>
+              </li>
+              <li>
+                <a onClick={() => handleFilter("Call")}>Call</a>
+              </li>
+              <li>
+                <a onClick={() => handleFilter("Text")}>Text</a>
+              </li>
+              <li>
+                <a onClick={() => handleFilter("Video")}>Video</a>
+              </li>
+            </ul>
+          </div>
+          <div className="mt-10">
+            {historyLog.map((log) => (
+              <div
+                key={log?.id}
+                className="bg-gray-100 p-4 mb-2 rounded-md flex gap-5"
+              >
+                <div className="flex justify-center items-center text-3xl w-14 rounded-full bg-gray-100 text-white">
+                  {log?.emoji}
+                </div>
+                <div className="space-y-2">
+                  <p className="text-lg text-gray-600">
+                    <span className="text-black font-blod">{log?.type}</span>{" "}
+                    with <span>{log?.name}</span>
+                  </p>
+                  <p className="text-lg text-gray-600">{log?.date}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </main>
   );
 };
 
